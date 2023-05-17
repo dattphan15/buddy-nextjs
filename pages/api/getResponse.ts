496c0ef8e8340d61e50b11aa10aa93cc869180b8
Kevin Phan <ken.phan@live.ca>
2023-05-18 01:56:54 +0700

    add error handling methods to api

--- pages/api/getResponse.ts
+++ pages/api/getResponse.ts
@@ -3,25 +3,40 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 // Create an async function for your handler
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
-  // Extract the message from the request body
-  const { message } = req.body;
+    try {
+        // Extract the message from the request body
+        const { message } = req.body;
+        
+        if (!message) {
+          console.error('No message provided in request');
+          return res.status(400).json({ error: 'No message provided in request' });
+        }
 
-  // Initialize OpenAI configuration
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+        // Initialize OpenAI configuration
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
 
-  const openai = new OpenAIApi(configuration);
+        const openai = new OpenAIApi(configuration);
 
-  // Generate a response from OpenAI
-  const gptResponse = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: message,
-    temperature: 0.5,
-    max_tokens: 100,
-  });
+        // Generate a response from OpenAI
+        const gptResponse = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: message,
+            temperature: 0,
+            max_tokens: 100,
+        });
 
-  // Respond with the AI's message
-  res.status(200).json({ message: gptResponse.data.choices[0].text });
+        // Log the response
+        console.log('GPT-3 Response:', gptResponse.data.choices[0].text);
+
+        // Respond with the AI's message
+        res.status(200).json({ message: gptResponse.data.choices[0].text });
+    } catch (error) {
+        // Log the error
+        console.error('Error processing request:', error);
+        
+        // Respond with an error message
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
 }
